08fbed6dd4652e2003cb195fa95632d693e5731c
txgcwm <txgcwm@txgcwmdeMacBook-Pro.local>
2016-09-29 00:21:33 +0800

    update code

--- base/src/CircularBuffer.cpp
+++ base/src/CircularBuffer.cpp
@@ -9,69 +9,71 @@ namespace Base {
 CircularBuffer::CircularBuffer(size_t capacity)
 : m_beg_index(0)
 , m_end_index(0)
-, size_(0)
-, capacity_(capacity)
+, m_size(0)
+, m_capacity(capacity)
 {
-	data_ = new char[capacity];
+	m_data = new char[capacity];
 }
 
 CircularBuffer::~CircularBuffer()
 {
-	delete [] data_;
+	delete [] m_data;
 }
 
 size_t CircularBuffer::write(const char *data, size_t bytes)
 {
-	if (bytes == 0) 
-		return 0;
+	size_t capacity = m_capacity;
+	size_t bytes_to_write = std::min(bytes, capacity - m_size);
 
-	size_t capacity = capacity_;
-	size_t bytes_to_write = std::min(bytes, capacity - size_);
+  if(bytes_to_write == 0) {
+    return 0;
+  }
 
 	// Write in a single step
 	if (bytes_to_write <= capacity - m_end_index) {
-    	memcpy(data_ + m_end_index, data, bytes_to_write);
+    	memcpy(m_data + m_end_index, data, bytes_to_write);
     	m_end_index += bytes_to_write;
     	if (m_end_index == capacity) {
       		m_end_index = 0;
 		}
   	} else { // Write in two steps
     	size_t size_1 = capacity - m_end_index;
-    	memcpy(data_ + m_end_index, data, size_1);
+    	memcpy(m_data + m_end_index, data, size_1);
     	size_t size_2 = bytes_to_write - size_1;
-    	memcpy(data_, data + size_1, size_2);
+    	memcpy(m_data, data + size_1, size_2);
     	m_end_index = size_2;
   	}
 
-  	size_ += bytes_to_write;
+  	m_size += bytes_to_write;
 
   	return bytes_to_write;
 }
 
 size_t CircularBuffer::read(char *data, size_t bytes)
 {
-  	if (bytes == 0)
-    	return 0;
+  	size_t capacity = m_capacity;
+  	size_t bytes_to_read = std::min(bytes, m_size);
 
-  	size_t capacity = capacity_;
-  	size_t bytes_to_read = std::min(bytes, size_);
+    if(bytes_to_read == 0) {
+      return 0;
+    }
 
   	// Read in a single step
   	if (bytes_to_read <= capacity - m_beg_index) {
-    	memcpy(data, data_ + m_beg_index, bytes_to_read);
+    	memcpy(data, m_data + m_beg_index, bytes_to_read);
     	m_beg_index += bytes_to_read;
     	if (m_beg_index == capacity) {
       		m_beg_index = 0;
 		}
   	} else { // Read in two steps
     	size_t size_1 = capacity - m_beg_index;
-    	memcpy(data, data_ + m_beg_index, size_1);
+    	memcpy(data, m_data + m_beg_index, size_1);
     	size_t size_2 = bytes_to_read - size_1;
-    	memcpy(data + size_1, data_, size_2);
+    	memcpy(data + size_1, m_data, size_2);
     	m_beg_index = size_2;
   	}
 
-  	size_ -= bytes_to_read;
+  	m_size -= bytes_to_read;
 
   	return bytes_to_read;
 }
--- base/src/CircularBuffer.h
+++ base/src/CircularBuffer.h
@@ -10,16 +10,19 @@ public:
   	CircularBuffer(size_t capacity);
   	~CircularBuffer();
 
-  	size_t size() const { return size_; }
-  	size_t capacity() const { return capacity_; }
+  	size_t size() const { return m_size; }
+  	size_t capacity() const { return m_capacity; }
   	// Return number of bytes written.
   	size_t write(const char *data, size_t bytes);
   	// Return number of bytes read.
   	size_t read(char *data, size_t bytes);
 
 private:
-  	size_t m_beg_index, m_end_index, size_, capacity_;
-  	char *data_;
+  	size_t 	m_beg_index;
+  	size_t 	m_end_index;
+  	size_t 	m_size;
+  	size_t 	m_capacity;
+  	char 	*data_;
 };
 
 
