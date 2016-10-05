6224bcf35994fa1117479d5858848e0f097b2022
txgcwm <txgcwm@txgcwm.(none)>
2016-10-06 00:09:11 +0800

    add zbar parse QRCode iamge code

---
+++ image/include/QRCodeDecode.h
@@ -0,0 +1,11 @@
+#ifndef __QRCODEDECODE_H__
+#define __QRCODEDECODE_H__
+
+namespace Image {
+
+bool ParseQrcodeImage(void *raw, int width, int height);
+
+} // end namespace
+
+
+#endif
---
+++ opensource/zbar/param.mk
@@ -0,0 +1,3 @@
+FILENAME=zbar-0.10.tar.bz2
+DIRNAME=zbar-0.10
+DOWNLOADURL=http://101.96.10.46/downloads.sourceforge.net/project/zbar/zbar/0.10/zbar-0.10.tar.bz2
