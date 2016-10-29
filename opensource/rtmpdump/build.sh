f2c5ea9eba9bd20f1031fe69d62d5444b0ff6d4e
txgcwm <txgcwm@163.com>
2016-10-30 00:00:00 +0800

    add rtmp code

--- opensource/rtmpdump/build.sh
+++ opensource/rtmpdump/build.sh
@@ -13,5 +13,9 @@ cd ${DIRNAME}
 make
 cd ..
 
+
+cp ${CURDIR}/rtmpdump/librtmp/amf.h ${CURDIR}/../prebuild/include/rtmpdump
+cp ${CURDIR}/rtmpdump/librtmp/http.h ${CURDIR}/../prebuild/include/rtmpdump
+cp ${CURDIR}/rtmpdump/librtmp/log.h ${CURDIR}/../prebuild/include/rtmpdump
 cp ${CURDIR}/rtmpdump/librtmp/rtmp.h ${CURDIR}/../prebuild/include/rtmpdump
 cp ${CURDIR}/rtmpdump/librtmp/librtmp.a ${CURDIR}/../prebuild/libs
