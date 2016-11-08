cc75001980c32f59248a96ebbf62d420fbaf22e2
wm8996 <txgcwm@163.com>
2016-11-08 13:10:26 +0800

    rm code

--- avcodec/flv/src/FlvDumpFile.cpp
+++ avcodec/flv/src/FlvDumpFile.cpp
@@ -117,110 +117,8 @@ int DumpFlv(CFlv *pFlv, const std::string &path)
 		u_char *pTagData = (*it_tag)->GetTagData();
 		uint32_t datasize = (*it_tag)->GetDataSize();
 
-		//check duplicate start code
-		if ((*it_tag)->GetType() == 0x09 && *(pTagData + 1) == 0x01) {
-#if 0
-			bool duplicate = false;
-			u_char *pStartCode = pTagData + 5 + nNalUnitLength;
-
-			unsigned nalu_len = 0;
-			u_char *p_nalu_len = (u_char *)&nalu_len;
-
-			switch (nNalUnitLength) {
-			case 4:
-				nalu_len = ShowU32(pTagData + 5);
-				break;
-
-			case 3:
-				nalu_len = ShowU24(pTagData + 5);
-				break;
-
-			case 2:
-				nalu_len = ShowU16(pTagData + 5);
-				break;
-
-			default:
-				nalu_len = ShowU8(pTagData + 5);
-				break;
-			}
-
-			u_char *pStartCodeRecord = pStartCode;
-			int i;
-
-			for (i = 0; i < datasize - 5 - nNalUnitLength - 4; ++i) {
-
-				if (pStartCode[i] == 0x00 && pStartCode[i + 1] == 0x00
-					&& pStartCode[i + 2] == 0x00 && pStartCode[i + 3] == 0x01) {
-
-					if (pStartCode[i + 4] == 0x67) {
-						printf("duplicate sps found!\n");
-						i += 4;
-						continue;
-					} else if (pStartCode[i + 4] == 0x68) {
-						printf("duplicate pps found!\n");
-						i += 4;
-						continue;
-					} else if (pStartCode[i + 4] == 0x06) {
-						printf("duplicate sei found!\n");
-						i += 4;
-						continue;
-					} else {
-						i += 4;
-						printf("offset=%d\n", i);
-						duplicate = true;
-						break;
-					}
-				}
-			}
-
-			if (duplicate) {
-				nalu_len -= i;
-				datasize -= i;
-				u_char *p = (u_char *)&(datasize);
-				pTagHeader[1] = p[2];
-				pTagHeader[2] = p[1];
-				pTagHeader[3] = p[0];
-
-				f.write((char *)pTagHeader, 11);
-
-				switch (nNalUnitLength) {
-				case 4:
-					*(pTagData + 5) = p_nalu_len[3];
-					*(pTagData + 6) = p_nalu_len[2];
-					*(pTagData + 7) = p_nalu_len[1];
-					*(pTagData + 8) = p_nalu_len[0];
-					break;
-
-				case 3:
-					*(pTagData + 5) = p_nalu_len[2];
-					*(pTagData + 6) = p_nalu_len[1];
-					*(pTagData + 7) = p_nalu_len[0];
-					break;
-
-				case 2:
-					*(pTagData + 5) = p_nalu_len[1];
-					*(pTagData + 6) = p_nalu_len[0];
-					break;
-
-				default:
-					*(pTagData + 5) = p_nalu_len[0];
-					break;
-				}
-
-				f.write((char *)pTagData, pStartCode - pTagData);
-				f.write((char *)pStartCode + i, datasize - (pStartCode - pTagData));
-			} else {
-				f.write((char *)pTagHeader, 11);
-				f.write((char *)pTagData, datasize);
-			}
-#else
-			f.write((char *)pTagHeader, 11);
-			f.write((char *)pTagData, datasize);
-#endif
-		} else {
-			f.write((char *)pTagHeader, 11);
-			f.write((char *)pTagData, datasize);
-		}
+		f.write((char *)pTagHeader, 11);
+		f.write((char *)pTagData, datasize);
 
 		nLastTagSize = 11 + datasize;
 	}
@@ -232,5 +130,104 @@ int DumpFlv(CFlv *pFlv, const std::string &path)
 
 	return 0;
 }
+
+
+#if 0
+if ((*it_tag)->GetType() == 0x09 && *(pTagData + 1) == 0x01) {
+	bool duplicate = false;
+	u_char *pStartCode = pTagData + 5 + nNalUnitLength;
+
+	unsigned nalu_len = 0;
+	u_char *p_nalu_len = (u_char *)&nalu_len;
+
+	switch (nNalUnitLength) {
+	case 4:
+		nalu_len = ShowU32(pTagData + 5);
+		break;
+
+	case 3:
+		nalu_len = ShowU24(pTagData + 5);
+		break;
+
+	case 2:
+		nalu_len = ShowU16(pTagData + 5);
+		break;
+
+	default:
+		nalu_len = ShowU8(pTagData + 5);
+		break;
+	}
+
+	u_char *pStartCodeRecord = pStartCode;
+	int i;
+
+	for (i = 0; i < datasize - 5 - nNalUnitLength - 4; ++i) {
+
+		if (pStartCode[i] == 0x00 && pStartCode[i + 1] == 0x00
+			&& pStartCode[i + 2] == 0x00 && pStartCode[i + 3] == 0x01) {
+
+			if (pStartCode[i + 4] == 0x67) {
+				printf("duplicate sps found!\n");
+				i += 4;
+				continue;
+			} else if (pStartCode[i + 4] == 0x68) {
+				printf("duplicate pps found!\n");
+				i += 4;
+				continue;
+			} else if (pStartCode[i + 4] == 0x06) {
+				printf("duplicate sei found!\n");
+				i += 4;
+				continue;
+			} else {
+				i += 4;
+				printf("offset=%d\n", i);
+				duplicate = true;
+				break;
+			}
+		}
+	}
+
+	if (duplicate) {
+		nalu_len -= i;
+		datasize -= i;
+		u_char *p = (u_char *)&(datasize);
+		pTagHeader[1] = p[2];
+		pTagHeader[2] = p[1];
+		pTagHeader[3] = p[0];
+
+		f.write((char *)pTagHeader, 11);
+
+		switch (nNalUnitLength) {
+		case 4:
+			*(pTagData + 5) = p_nalu_len[3];
+			*(pTagData + 6) = p_nalu_len[2];
+			*(pTagData + 7) = p_nalu_len[1];
+			*(pTagData + 8) = p_nalu_len[0];
+			break;
+
+		case 3:
+			*(pTagData + 5) = p_nalu_len[2];
+			*(pTagData + 6) = p_nalu_len[1];
+			*(pTagData + 7) = p_nalu_len[0];
+			break;
+
+		case 2:
+			*(pTagData + 5) = p_nalu_len[1];
+			*(pTagData + 6) = p_nalu_len[0];
+			break;
+
+		default:
+			*(pTagData + 5) = p_nalu_len[0];
+			break;
+		}
+
+		f.write((char *)pTagData, pStartCode - pTagData);
+		f.write((char *)pStartCode + i, datasize - (pStartCode - pTagData));
+	} else {
+		f.write((char *)pTagHeader, 11);
+		f.write((char *)pTagData, datasize);
+	}
+}
+#endif
 
 
