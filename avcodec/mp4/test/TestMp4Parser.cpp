40c2c69ef524aad30f56942f5c83ae3260c10877
txgcwm <txgcwm@163.com>
2016-11-12 23:08:32 +0800

    modify code

--- avcodec/mp4/test/TestMp4Parser.cpp
+++ avcodec/mp4/test/TestMp4Parser.cpp
@@ -5,28 +5,29 @@
 
 
 
-class CDemo : public CMp4FileCallBack 
+class CDemo : public CMp4ParserCallBack 
 {
 public:
 	CDemo();
 	virtual ~CDemo();
 
-	uint16_t StartParse();
-	uint16_t StopParse();
+	bool StartParse();
+	bool StopParse();
 
 public:
 	virtual void OnVideoCallBack(void *thiz, const u_char *pPacketPtr, const uint16_t wPackLen, const bool bKeyFrame);
-	virtual void OnAudioCallBack(void *thiz, const u_char *pPacketPtr, const uint16_t wPackLen);
+	virtual void OnAudioCallBack(void *thiz,  const u_char *pHeader, const uint16_t wHeaderSize, 
+								const u_char *pPacketPtr, const uint16_t wPackLen);
 
 private:
-	CMp4File *pFile;
+	CMp4Parser *pFile;
 	FILE *pOutVideoFile;
 	FILE *pOutAudioFile;
 };
 
 CDemo::CDemo()
 {
-	pFile = new CMp4File(this, "test.mp4");
+	pFile = new CMp4Parser(this, "test.mp4");
 
 	pOutVideoFile = fopen("out.h264", "wb");
 	pOutAudioFile = fopen("out.aac", "wb");
@@ -40,53 +41,39 @@ CDemo::~CDemo()
 	}
 }
 
-uint16_t CDemo::StartParse()
+bool CDemo::StartParse()
 {
-	if (NULL == pFile) {
-		return 1;
-	}
-
-	return pFile->StartParse();
+	return pFile->Start();
 }
 
-uint16_t CDemo::StopParse()
+bool CDemo::StopParse()
 {
-	if (NULL == pFile) {
-		return 1;
-	}
-
-	return pFile->StopParse();
+	return pFile->Stop();
 }
 
 void CDemo::OnVideoCallBack(void *thiz, const u_char *pPacketPtr, const uint16_t wPackLen, const bool bKeyFrame)
 {
-	if (pOutVideoFile && pPacketPtr) {
-		fwrite(pPacketPtr, 1, wPackLen, pOutVideoFile);
-	}
+	fwrite(pPacketPtr, 1, wPackLen, pOutVideoFile);
 }
 
-void CDemo::OnAudioCallBack(void *thiz, const u_char *pPacketPtr, const uint16_t wPackLen)
+void CDemo::OnAudioCallBack(void *thiz,  const u_char *pHeader, const uint16_t wHeaderSize, 
+							const u_char *pPacketPtr, const uint16_t wPackLen)
 {
-	if (pOutAudioFile && pPacketPtr) {
-		fwrite(pPacketPtr, 1, wPackLen, pOutAudioFile);
-	}
+	fwrite(pHeader, 1, wHeaderSize, pOutAudioFile);
+	fwrite(pPacketPtr, 1, wPackLen, pOutAudioFile);
 }
 
 int main(int argc, char **argv)
 {
-	uint16_t wRet = MP4PARSER_NO_ERROR;
-
 	CDemo *pDemo = new CDemo();
 	if (NULL == pDemo) {
 		return -1;
 	}
 
-	wRet = pDemo->StartParse();
-	if (MP4PARSER_NO_ERROR != wRet) {
-		return -1;
-	}
+	pDemo->StartParse();
 
 	pDemo->StopParse();
+
 	delete pDemo;
 	pDemo = NULL;
 
